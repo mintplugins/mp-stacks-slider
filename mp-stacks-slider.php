2e10456b6baff6d5c9645f190fd9f1f83ec8efc4
Phil Johnston <support@mintplugins.com>
2016-01-06 11:14:38 -0500

    1.0.1.0 Verion Bump

--- mp-stacks-slider.php
+++ mp-stacks-slider.php
@@ -3,7 +3,7 @@
 Plugin Name: MP Stacks + Slider
 Plugin URI: http://mintplugins.com/plugins/mp-stacks-slider
 Description: Show an image/video slider on any page, at any time, anywhere on your website. Just put make a brick using “MP Stacks”, put the stack on a page, and set the brick’s Content-Type to be “Slider”.
-Version: 1.0.0.9
+Version: 1.0.1.0
 Author: Mint Plugins
 Author URI: http://mintplugins.com
 Text Domain: mp_stacks_slider
@@ -11,7 +11,7 @@ Domain Path: languages
 License: GPL2
 */
 
-/*  Copyright 2015  Phil Johnston  (email : phil@mintplugins.com)
+/*  Copyright 2016  Phil Johnston  (email : phil@mintplugins.com)
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License, version 2, as 
@@ -34,7 +34,7 @@ License: GPL2
 */
 // Plugin version
 if( !defined( 'MP_STACKS_SLIDER_VERSION' ) )
-	define( 'MP_STACKS_SLIDER_VERSION', '1.0.0.9' );
+	define( 'MP_STACKS_SLIDER_VERSION', '1.0.1.0' );
 
 // Plugin Folder URL
 if( !defined( 'MP_STACKS_SLIDER_PLUGIN_URL' ) )
--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Donate link: http://mintplugins.com/
 Tags: message bar, header
 Requires at least: 3.5
 Tested up to: 4.4
-Stable tag: 1.0.0.9
+Stable tag: 1.0.1.0
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -32,6 +32,10 @@ See full instructions at http://mintplugins.com/doc/mp-stacks
 
 
 == Changelog ==
+
+= 1.0.1.0 = January 6, 2016
+* Update FlexSlider to 2.6.0
+* Temporarily set all sliders to fade mode until responsive slide issue is resolved.
 
 = 1.0.0.9 = December 30, 2015
 * Make sure that the images in the slider with links don't get the opacity settings that the navigation dots do.
